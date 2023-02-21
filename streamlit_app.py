f0aa6bec0f43868d2970bd0b68b737b5642d5eed
Mark F Snow <marksnowf@gmail.com>
2023-02-21 13:32:59 +0800

    added emojis added avocado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Bueberry Oatmeal')
-streamlit.text('Kale & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥣 Omega 3 & Bueberry Oatmeal')
+streamlit.text('🥗 Kale & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
