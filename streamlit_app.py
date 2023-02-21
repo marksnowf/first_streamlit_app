a1703c8cdd53a6a612c009ac508b5c56a4f88531
Mark F Snow <marksnowf@gmail.com>
2023-02-21 13:18:47 +0800

    added a heading added some contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Bueberry Oatmeal')
+streamlit.text('Kale & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
