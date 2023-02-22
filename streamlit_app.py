2011c12dc6118b01a60bd23ff0482c00f7a5206b
Mark F Snow <marksnowf@gmail.com>
2023-02-22 15:21:28 +0800

    Try fixing error

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ my_cur = my_cnx.cursor()
 
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-my_cur.execute("select * from fruit_load_list")
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
