0c3f0b5ebe781e436267dedba7509fb8746a534e
Mark F Snow <marksnowf@gmail.com>
2023-02-22 16:06:04 +0800

    added try statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit
+import requests
+import pandas
+import snowflake.connector
+
+from urllib.error import URLError
+
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -14,7 +20,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -33,24 +38,32 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+try:
+	fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+		fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+		streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+	streamlit.error()
+
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output it to the screen as table
-streamlit.dataframe(fruityvice_normalized)
 
 
 
 #streamlit.text(fruityvice_response.json())
 
-import snowflake.connector
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -62,7 +75,8 @@ my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', '')
 
 if add_my_fruit is not None:
-	streamlit.write('Thanks for adding ', add_my_fruit)
+	streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')
 
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
