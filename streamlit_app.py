10210501b38cfb2cdeb5ca0b3ae28655e33712b2
Mark F Snow <marksnowf@gmail.com>
2023-02-22 15:27:24 +0800

    Try fixing error

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("USE ROLE ACCOUTADMIN")
+my_cur.execute("USE ROLE ACCOUNTADMIN")
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 
 
