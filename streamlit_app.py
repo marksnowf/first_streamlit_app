dd53a1c5558f00a823ea95f514e83343fc40cdd9
Mark F Snow <marksnowf@gmail.com>
2023-02-22 15:34:34 +0800

    added dataframe for fetch all

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ my_cur.execute("USE ROLE ACCOUNTADMIN")
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 
 
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
